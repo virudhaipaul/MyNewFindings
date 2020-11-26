3ea57149ce855f7f130836e46ac50cedb4ca5160
virudhaipaul <paul.bca@gmail.com>
2020-11-26 23:49:59 +0530

    Update byteArrayToHexFormat.java

--- byteArrayToHexFormat.java
+++ byteArrayToHexFormat.java
@@ -6,3 +6,13 @@
          hexString.append(Integer.toHexString(0xFF & digest[i]));
       }
       System.out.println("Hex format : " + hexString.toString());    
+
+// example 2:
+byte[] decryptedMessage = rsaDecrypt.doFinal(encryptedMessage);
+
+            System.out.println("Decrypted data:");
+            for (int i = 0; i < decryptedMessage.length; ++i) {
+                System.out.printf(" %02X", decryptedMessage[i]);
+                if ((i + 1) % 8 == 0)
+                    System.out.println();
+            }
